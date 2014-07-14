60d393f218d563f84d03c4fb78a49801991136aa
NSS Student <mikeb_barreiro@yahoo.com>
2014-07-13 22:26:30 -0500

    Fixed errors

--- app.js
+++ app.js
@@ -1,51 +1,22 @@
 var prompt = require('sync-prompt').prompt
 var chalk = require('chalk');
 
-var lbs= 0, food=[], Cal=[]; sumCals = 0; weigh = 0;
+var weight = prompt('What is your weight? ');
+var gender = prompt('Your Gender? M/F ');
 
-var gender
-var weight = prompt('How much do you weigh? ');
-  weight = parseInt(weight);
-  gender = prompt('What is your gender? F/M? ');
+weight = parseInt(weight);
+var foods = [];
+var total = 0;
 
-var eat
-var input
-
-  while(input != 'q') {
-   eat = prompt('What did you eat? or (q)uit? ');
-  food.push(eat);
-var calnum = prompt('How many calories? ');
-  calnum = parseInt(calnum);
-  Cal.push(calnum);
-  input = prompt('Click any key to enter another food or (q)uit ')
-
-  }
-
-
-console.log('Devoured Foods: ' + chalk.blue( food));
-
-var i
-for( i = 0; i < Cal.length; i++) {
-  sumCals += Cal[i]
-}
-console.log('You took in ' + chalk.red(sumCals) + ' calories');
-
-
-var add
-add = parseFloat(add)
-
-if(gender === 'M' && sumCals >= 4000) {
-   add =  sumCals/4000;
-}else if(gender === 'F' && sumcals >= 3000){
- add = sumcals/3000
+var option = prompt('(f)ood or (q)uit ');
+while(option != 'q'){
+  var food = prompt ('Food you ate: ');
+  var calories = prompt('How many calories was it? ');
+  
+  total += parseInt(calories);
+  foods.push(food);
+  option = prompt ('(f)ood or (q)uit ');
 }
 
-
-
-console.log('You have gained ' + chalk.blue(add.toFixed(10)) + ' pounds. Now you should weight ' + chalk.blue((weigh+add).toFixed(1)) )
-
-
-
-
-
-
+weight += gender === 'f' ? total/3000 : total /4000;
+console.log('You have eaten: ' + chalk.cyan( foods) + '.You now have gained ' + chalk.red(weight.toFixed(1)) + ' lbs');
